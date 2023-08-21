e5258341c430a9905e43ffbd6027fe68c69f11bc
Hacktronics India <support@hacktronics.co.in>
2023-08-22 01:01:51 +0530

    new version

--- assets/ios.transition-legacy-4fd37588.js
+++ assets/ios.transition-legacy-4fd37588.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-b6e4a9c5.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.v,n=e.x}],execute:function(){
+System.register(["./index-legacy-f67c058a.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.v,n=e.x}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
