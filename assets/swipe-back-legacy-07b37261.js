bd1ce8dc3cf497b9d791bb8ad68b8b2c2e8741ed
Hacktronics India <support@hacktronics.co.in>
2023-08-27 23:53:20 +0530

    new version

--- assets/swipe-back-legacy-07b37261.js
+++ assets/swipe-back-legacy-07b37261.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-97803f00.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-e47a6f46.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
